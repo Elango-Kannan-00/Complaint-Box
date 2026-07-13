5186d05623b9d6c07981b6e453ea612316372ad2
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-07-13 23:58:14 +0530

    Add complaint response dto.

---
+++ backend/src/main/java/com/cms/backend/dto/complaint/ComplaintResponseDto.java
@@ -0,0 +1,35 @@
+package com.cms.backend.dto.complaint;
+
+import java.sql.Timestamp;
+
+import com.cms.backend.enums.ComplaintStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ComplaintResponseDto {
+
+    private Long complaintId;
+
+    private String complaintTitle;
+
+    private String complaintDescription;
+
+    private ComplaintStatus complaintStatus;
+
+    private String studentName;
+
+    private String departmentName;
+
+    private Timestamp createdAt;
+
+    private Timestamp updatedAt;
+
+    private String feedback;
+}

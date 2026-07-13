edd7cbc5b5e0854b055ceb7a5a34c8a8581b689e
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-07-13 23:38:51 +0530

    Add repository interfaces for Complaint, Department, and User entities

---
+++ backend/src/main/java/com/cms/backend/repository/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.cms.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cms.backend.entity.Department;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long>{ }
---
+++ backend/src/main/java/com/cms/backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.cms.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cms.backend.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> { }

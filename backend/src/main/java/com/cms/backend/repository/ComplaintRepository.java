ff5d37bf50e77f6f0105827eeaa0ba0086a378aa
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-07-16 20:58:46 +0530

    Format ComplaintRepository interface for consistent styling

--- backend/src/main/java/com/cms/backend/repository/ComplaintRepository.java
+++ backend/src/main/java/com/cms/backend/repository/ComplaintRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cms.backend.entity.Complaint;
 
-public interface ComplaintRepository extends JpaRepository<Complaint, Long> { }
+public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
+}

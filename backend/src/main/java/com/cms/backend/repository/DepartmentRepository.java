e473c2c495864a7ca4348f552f97ec746f4a93d8
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-07-16 18:56:46 +0530

    Remove unused DepartmentRepository interface

--- backend/src/main/java/com/cms/backend/repository/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cms.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cms.backend.entity.Department;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long>{ }

e5968c09cc8c132ad93ab4d27030a3b8afce57fe
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-07-16 20:58:25 +0530

    Add AcademicDepartmentDto class with fields for department ID and name

---
+++ backend/src/main/java/com/cms/backend/dto/department/AcademicDepartmentDto.java
@@ -0,0 +1,15 @@
+package com.cms.backend.dto.department;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AcademicDepartmentDto {
+    private Long academicDepartmentId;
+    private String academicDepartmentName;
+}

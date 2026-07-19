0a1f7c80244f4df615efe758b1999da5d855aaec
Elango Kannan <2k23cse040@kiot.ac.in>
2026-07-19 23:34:24 +0530

    Add StudentComplaintUpdateDto for updating student complaints

---
+++ backend/src/main/java/com/cms/backend/dto/complaint/StudentComplaintUpdateDto.java
@@ -0,0 +1,19 @@
+package com.cms.backend.dto.complaint;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentComplaintUpdateDto {
+    @NotBlank
+    @Size(max = 100)
+    private String complaintTitle;
+
+    @NotBlank
+    @Size(max = 1000)
+    private String complaintDescription;
+}

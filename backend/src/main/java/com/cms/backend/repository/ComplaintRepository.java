f0678e0670895d1bcb750ee0e278c20fc88168ec
Elango Kannan <2k23cse040@kiot.ac.in>
2026-07-19 23:34:29 +0530

    Add methods to ComplaintRepository for fetching complaints by student and HOD user IDs

--- backend/src/main/java/com/cms/backend/repository/ComplaintRepository.java
+++ backend/src/main/java/com/cms/backend/repository/ComplaintRepository.java
@@ -1,8 +1,12 @@
 package com.cms.backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cms.backend.entity.Complaint;
 
 public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
+    List<Complaint> findByStudentUserId(Long userId);
+    List<Complaint> findByComplaintDepartmentHodUserId(Long hodId);
 }

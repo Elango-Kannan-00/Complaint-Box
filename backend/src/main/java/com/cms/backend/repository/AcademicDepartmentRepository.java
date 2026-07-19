495212076d3dc632ef9c8ef42ae19d183dceaadb
Elango Kannan <2k23cse040@kiot.ac.in>
2026-07-20 00:46:04 +0530

    Add JavaDoc comment for application.

--- backend/src/main/java/com/cms/backend/repository/AcademicDepartmentRepository.java
+++ backend/src/main/java/com/cms/backend/repository/AcademicDepartmentRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cms.backend.entity.AcademicDepartment;
 
+/**
+ * Repository for `AcademicDepartment` entities.
+ */
 public interface AcademicDepartmentRepository extends JpaRepository<AcademicDepartment, Long> {
 }
